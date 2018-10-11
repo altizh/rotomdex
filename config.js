2d80d2ce38e6efb6173ee0d3bd906ab8608ec739
Alan Zhu <zhu.alan.t@gmail.com>
2018-10-10 22:16:45 -0700

    updated test server id

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
   "ownerID": "142501740212649984",
   "botID": "444302804442677248",
-  "testGuildID" : "378389280332054529",
+  "testGuildID" : "499809422521008139",
   "token": process.env.TOKEN,
   "inviteURL": "https://discordapp.com/oauth2/authorize?client_id=444302804442677248&permissions=67488832&scope=bot",
   "defaultSettings" : {
